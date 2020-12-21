ad8ee097a6c6e71a622b075127d8ac1d1fa3d4f0
tudekai <tudekai@qq.com>
2020-12-21 23:51:49 +0800

    提交空工程

---
+++ provider-payment/src/main/java/cc/linglan/study/springcloud/PayMentApp.java
@@ -0,0 +1,14 @@
+package cc.linglan.study.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class PayMentApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayMentApp.class,args);
+    }
+}
